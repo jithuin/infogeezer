835ce097536271ddef33ef88a763ec397ad857d3
leonzrt <leonzrt@gmail.com>
2012-03-30 16:50:16 +0000

    Added: FileIndexer

---
+++ FileIndexer/FileIndexer/DirectoryStructure.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+using System.IO;
+using System.Diagnostics;
+
+namespace FileIndexer
+{
+    public class DirectoryStructure
+    {
+        public DirectoryStructure()
+        {
+
+        }
+
+        public void ScanDirectoryStructure(BackgroundWorker bw, DoWorkEventArgs e)
+        {
+            DirectoryInfo di = e.Argument as DirectoryInfo;
+            if (di != null)
+                _listOfDirectories.Add(new ScannableDirectoryInfo(di));
+            for (int i = 0; i < _listOfDirectories.Count && bw.CancellationPending != true; i++)
+            {
+                if (ScanThisDirectory(_listOfDirectories[i]))
+                {
+                    bw.ReportProgress(0, new ScanState(_listOfDirectories[i].DirectoryInfo.FullName, _listOfDirectories.Count + NumberOfFiles));
+                }
+
+            }
+            if (bw.CancellationPending)
+                e.Cancel = true;
+
+        }
+        List<ScannableDirectoryInfo> _listOfDirectories = new List<ScannableDirectoryInfo>();
+        long NumberOfFiles = 0;
+        public bool ScanThisDirectory(ScannableDirectoryInfo sdi)
+        {
+            bool _errorFlag = false;
+            if (sdi.Scanned == true)
+                return false;
+            try
+            {
+                _listOfDirectories.AddRange(sdi.DirectoryInfo
+                .GetDirectories()
+                .Select<DirectoryInfo, ScannableDirectoryInfo>(new Func<DirectoryInfo, ScannableDirectoryInfo>(
+                    (di) =>
+                    {
+                        return new ScannableDirectoryInfo(di);
+                    }
+                    )));
+                NumberOfFiles += sdi.DirectoryInfo.GetFiles().Count();
+            }
+            catch (Exception ex)
+            {
+                Trace.TraceWarning(ex.Message);
+                _errorFlag = true;
+            }
+
+            return !_errorFlag;
+        }
+    }
+
+    #region Scan State
+    public interface IScanState
+    {
+        long NumberOfScannedNodes { get; }
+        String CurrentNode { get; }
+    }
+
+    public class ScanState : IScanState
+    {
+        public ScanState(String currentNode_in, long numberOfScannedNodes_in)
+        {
+            this.CurrentNode = currentNode_in;
+            this.NumberOfScannedNodes = numberOfScannedNodes_in;
+        }
+
+        #region IScanState Members
+
+        public long NumberOfScannedNodes
+        {
+            get;
+            protected set;
+        }
+
+        public string CurrentNode
+        {
+            get;
+            protected set;
+        }
+
+        #endregion
+    }
+    #endregion
+
+    #region Scannable Directory Info
+    public interface IScannable
+    {
+        bool Scanned { get; set; }
+    }
+
+    public class ScannableDirectoryInfo : IScannable
+    {
+        public ScannableDirectoryInfo()
+        {
+
+        }
+        public ScannableDirectoryInfo(DirectoryInfo di_in)
+        {
+            DirectoryInfo = di_in;
+        }
+
+        public DirectoryInfo DirectoryInfo { get; set; }
+
+        #region IScannable Members
+
+        public bool Scanned
+        {
+            get;
+            set;
+        }
+
+        #endregion
+    }
+    #endregion
+
+}
---
+++ FileIndexer/FileIndexer/FileIndexerWindow.xaml.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.ComponentModel;
+using System.IO;
+
+namespace FileIndexer
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class FileIndexerWindow : Window
+    {
+        public FileIndexerWindow()
+        {
+            InitializeComponent();
+            InitBackroundWorker();
+        }
+
+        private void InitBackroundWorker()
+        {
+            bw.DoWork += new DoWorkEventHandler(bw_DoWork);
+            bw.ProgressChanged += new ProgressChangedEventHandler(bw_ProgressChanged);
+            bw.RunWorkerCompleted += new RunWorkerCompletedEventHandler(bw_RunWorkerCompleted);
+            bw.WorkerReportsProgress = true;
+            bw.WorkerSupportsCancellation = true;
+        }
+        DirectoryStructure _dirStruct = new DirectoryStructure();
+
+        void bw_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Cancelled || e.Error != null)
+            {
+                _tbCurrentDirectory.Text += "\r\n Aborted";
+            }
+            else
+            {
+                _tbCurrentDirectory.Text += "\r\n Ready";
+                _isButton = false;
+                _btStartText.Text = "Scan";
+            }
+        }
+
+        void bw_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            IScanState state = e.UserState as IScanState;
+            if (state != null)
+            {
+                Dispatcher.BeginInvoke(new Action<IScanState>((st) => 
+                { 
+                    _tbCurrentDirectory.Text = String.Format("Current Node: {0}", st.CurrentNode );
+                    _tbNumberOfScannedNodes.Text = String.Format("\r\nScanned Nodes: {0:D10}", st.NumberOfScannedNodes);
+                }), state);
+            }
+        }
+
+        void bw_DoWork(object sender, DoWorkEventArgs e)
+        {
+            _dirStruct.ScanDirectoryStructure(bw, e);
+        }
+
+        BackgroundWorker bw = new BackgroundWorker();
+
+        bool _isButton = false;
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (!_isButton)
+            {
+                if (_tbRootPath.Text != "")
+                {
+                    DirectoryInfo di = new DirectoryInfo(_tbRootPath.Text);
+                    if (di.Exists)
+                    {
+                        bw.RunWorkerAsync(di);
+                        _isButton = true;
+                        _btStartText.Text = "Cancel";
+                    }
+                }
+            }
+            else
+            {
+                bw.CancelAsync();
+
+                _isButton = false;
+                _btStartText.Text = "Scan";
+            }
+        }
+
+        private void ExitMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void Grid_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        
+    }
+}
