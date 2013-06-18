20122c390030bd03953e444c5109a593bc0ddefd
leonzrt <leonzrt@gmail.com>
2013-06-18 16:41:36 +0000

    Modified: ViewModel

--- DecimalInternetClock/DecimalInternetClock/Clocks/ViewModel/BinaryHexDigitClockViewModel.cs
+++ DecimalInternetClock/DecimalInternetClock/Clocks/ViewModel/BinaryHexDigitClockViewModel.cs
@@ -9,63 +9,25 @@ namespace DecimalInternetClock
 {
     public class BinaryHexDigitClockViewModel : INotifyPropertyChanged
     {
+        #region Fields
         HexDigitClockModel _clock;
+        Dictionary<HexDigitClockModel.EUnits, BinaryHexDigitViewModel> _subViewModels;
+        #endregion Fields
+
+        #region Public Properties
 
         public DateTime Now
         {
             set
             {
                 _clock.Now = value;
-                UpdateSubViewModels();
-            }
-        }
-
-        public BinaryHexDigitClockViewModel()
-        {
-            _clock = new HexDigitClockModel();
-            InitSubViewModels();
-        }
-
-        private void InitSubViewModels()
-        {
-            _subViewModels = new Dictionary<HexDigitClockModel.EUnits, BinaryHexDigitViewModel>();
-            foreach (HexDigitClockModel.EUnits unit in EnumHelper.GetValues<HexDigitClockModel.EUnits>())
-            {
-                _subViewModels.Add(unit, new BinaryHexDigitViewModel());
-                _subViewModels[unit].PropertyChanged += new PropertyChangedEventHandler(BinaryHexDigitClockViewModel_PropertyChanged);
-            }
-        }
-
-        private void BinaryHexDigitClockViewModel_PropertyChanged(object sender, PropertyChangedEventArgs e)
-        {
-            UpdateFromSubViewModels();
-        }
-
-        private void UpdateFromSubViewModels()
-        {
-            foreach (HexDigitClockModel.EUnits unit in EnumHelper.GetValues<HexDigitClockModel.EUnits>())
-                if (_subViewModels[unit].Now != _clock[unit])
+                foreach (HexDigitClockModel.EUnits unit in EnumHelper.GetValues<HexDigitClockModel.EUnits>())
                 {
-                    _clock[unit] = _subViewModels[unit].Now;
+                    _subViewModels[unit].Now = _clock[unit];
                     OnPropertyChanged(unit);
                 }
-        }
-
-        private void UpdateSubViewModels()
-        {
-            foreach (HexDigitClockModel.EUnits unit in EnumHelper.GetValues<HexDigitClockModel.EUnits>())
-            {
-                _subViewModels[unit].Now = _clock[unit];
-                OnPropertyChanged(unit);
             }
         }
-
-        public void UpdateNow()
-        {
-            Now = DateTime.Now;
-        }
-
-        Dictionary<HexDigitClockModel.EUnits, BinaryHexDigitViewModel> _subViewModels;
 
         public BinaryHexDigitViewModel Hour
         {
@@ -98,6 +60,53 @@ namespace DecimalInternetClock
                 return _subViewModels[HexDigitClockModel.EUnits.Second];
             }
         }
+
+        #endregion Public Properties
+
+        #region Constructor
+
+        public BinaryHexDigitClockViewModel()
+        {
+            _clock = new HexDigitClockModel();
+            //Init Sub-View-Models
+            _subViewModels = new Dictionary<HexDigitClockModel.EUnits, BinaryHexDigitViewModel>();
+            foreach (HexDigitClockModel.EUnits unit in EnumHelper.GetValues<HexDigitClockModel.EUnits>())
+            {
+                _subViewModels.Add(unit, new BinaryHexDigitViewModel());
+                _subViewModels[unit].PropertyChanged += new PropertyChangedEventHandler(
+                    (sender, e) =>
+                    {
+                        foreach (HexDigitClockModel.EUnits _unit in EnumHelper.GetValues<HexDigitClockModel.EUnits>())
+                            if (_subViewModels[_unit].Now != _clock[_unit])
+                            {
+                                _clock[_unit] = _subViewModels[_unit].Now;
+                                OnPropertyChanged(_unit);
+                            }
+                    }
+                    );
+            }
+        }
+
+        #endregion Constructor
+
+        #region Methods
+
+        //private void BinaryHexDigitClockViewModel_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        //{
+        //    foreach (HexDigitClockModel.EUnits unit in EnumHelper.GetValues<HexDigitClockModel.EUnits>())
+        //        if (_subViewModels[unit].Now != _clock[unit])
+        //        {
+        //            _clock[unit] = _subViewModels[unit].Now;
+        //            OnPropertyChanged(unit);
+        //        }
+        //}
+
+        public void UpdateNow()
+        {
+            Now = DateTime.Now;
+        }
+
+        #endregion Methods
 
         #region INotifyPropertyChanged Members
 
